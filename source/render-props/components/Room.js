57508c60fe6f81079dd1dbcd0a0f332b42f0146b
Mashchenko Oleksandra <dremka@hotmail.com>
2019-03-20 23:11:08 +0200

    init

--- source/render-props/components/Room.js
+++ source/render-props/components/Room.js
@@ -1,11 +1,37 @@
 // Core
 import React, { Component } from 'react';
 
+// Components
+import Tracker from './Tracker';
+import Animals from './Animals';
+
+const Content = (props) => {
+    return (
+        <>
+            <Animals.Tile { ...props } />
+            <Animals.Mouse { ...props } />
+            <Animals.Cat { ...props } />
+        </>
+    );
+};
+
 export default class Room extends Component {
     render () {
         return (
             <div className = 'room'>
                 <h1>Поиграем в кошки-мышки!</h1>
+                {/* <Tracker
+                    render = { (coordinates) => {
+                        return <Content { ...coordinates } />;
+                    } }
+                /> */}
+                {/* <Tracker>
+                    { (coordinates) => {
+                        return <Content { ...coordinates } />;
+                    } }
+                </Tracker> */}
+                <Tracker component = { Content } />
+                />
             </div>
         );
     }
---
+++ source/render-props/components/Tracker.js
@@ -0,0 +1,38 @@
+// Core
+import React, { Component } from 'react';
+
+export default class Tracker extends Component {
+    state = {
+        x: 0,
+        y: 0,
+    };
+
+    _handleMouseMove = (event) => {
+        this.setState({
+            x: event.clientX,
+            y: event.clientY,
+        });
+    }
+
+    render () {
+        const { render, children, component } = this.props;
+
+        let result = null;
+
+        if (render) {
+            result = render(this.state);
+        }
+        if (children) {
+            result=children(this.state);
+        }
+        if (component) {
+            result = React.createElement(component, this.state);
+        }
+
+        return (
+            <div className = 'tracker' onMouseMove = { this._handleMouseMove }>
+                {result}
+            </div>
+        );
+    }
+}
